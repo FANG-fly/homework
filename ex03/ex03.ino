c42988a481529df1a085e4c01c64331ef6d675ea
FANG-fly <1277408906@qq.com>
2026-06-19 14:58:45 +0800

    ex03: 优化呼吸灯参数，加快呼吸速度并降低PWM频率

--- ex03/ex03.ino
+++ ex03/ex03.ino
@@ -1,26 +1,26 @@
-// ex03.ino - Commit 1: LED呼吸灯基础版
+// ex03.ino - Commit 2: LED呼吸灯优化版
 
 const int ledPin = 2;
 
-const int freq = 5000;
+const int freq = 1000;      // 降低PWM频率
 const int resolution = 8;
 
 void setup() {
   Serial.begin(115200);
   ledcAttach(ledPin, freq, resolution);
-  Serial.println("=== ex03 Commit 1: Breathing Basic ===");
+  Serial.println("=== ex03 Commit 2: Breathing Optimized ===");
 }
 
 void loop() {
   for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++) {
     ledcWrite(ledPin, dutyCycle);
-    delay(10);
+    delay(5);  // 加快呼吸速度
   }
   
   for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--) {
     ledcWrite(ledPin, dutyCycle);
-    delay(10);
+    delay(5);
   }
   
-  Serial.println("Breathing cycle completed");
+  Serial.println("Breathing cycle V2 completed - faster");
 }
